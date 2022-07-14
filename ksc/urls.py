8f1beea803ccabe060bd0392a2b1fde17e84b97a
sagar <sagarmalla08@gmail.com>
2022-07-14 13:10:43 +0545

    basic structure template inheritanc, Carousel and  logo in navbar added

--- ksc/urls.py
+++ ksc/urls.py
@@ -4,5 +4,7 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='KarnaliSportsClub Home'),
+    path('about/', views.about, name='About us'),
+    path('contact/', views.contact, name='About us'),
  
 ]
--- ksc/views.py
+++ ksc/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'ksc/index.html')
+
+def about(request):
+    return render(request, 'ksc/about.html')
+
+def contact(request):
+    return render(request, 'ksc/contact.html')
