56f2fef4cf96f0da7664c94d4fade80f9b0acf81
sagar <sagarmalla08@gmail.com>
2022-07-14 17:43:23 +0545

    testing...

--- ksc/views.py
+++ ksc/views.py
@@ -1,4 +1,5 @@
 from math import ceil
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import ImageSlider
@@ -6,7 +7,12 @@ from ksc.models import ImageSlider
 
 # Create your views here.
 def index(request):
-    return render(request, 'ksc/index.html')
+    ImageSliders = ImageSlider.objects.all()
+    print(ImageSliders)
+    n = len(ImageSliders)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'ImageSlider': ImageSliders}
+    return render(request, 'ksc/index.html', params)
 
 def about(request):
     return render(request, 'ksc/about.html')
